076724f6deb7b32d124598b5d77a48f43f20e670
Simovanev <simovanev@abv.bg>
2024-09-26 06:24:29 +0300

    Initial commit

--- src/main/java/springboot/jpaAdvancedMapping/JpaAdvancedMappingApplication.java
+++ src/main/java/springboot/jpaAdvancedMapping/JpaAdvancedMappingApplication.java
@@ -1,7 +1,10 @@
 package springboot.jpaAdvancedMapping;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaAdvancedMappingApplication {
@@ -9,5 +12,10 @@ public class JpaAdvancedMappingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaAdvancedMappingApplication.class, args);
 	}
-
+@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Welcome to Jpa Advanced Mapping");
+		};
+}
 }
