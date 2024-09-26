aeaa2a3f3ccfed0a8432540867e867f798336fcd
Simovanev <simovanev@abv.bg>
2024-09-26 05:54:43 +0300

    Initial commit

---
+++ src/main/java/springboot/jpaAdvancedMapping/JpaAdvancedMappingApplication.java
@@ -0,0 +1,13 @@
+package springboot.jpaAdvancedMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancedMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancedMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/jpaAdvancedMapping/JpaAdvancedMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.jpaAdvancedMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancedMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
