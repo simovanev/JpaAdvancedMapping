0e1a23420f9c4f54f4b04e1255177928fafe64ab
PC1237 <simovanev@abv.bg>
2024-09-27 12:50:55 +0300

    Create DAO

---
+++ src/main/java/springboot/jpaAdvancedMapping/dao/AppDAO.java
@@ -0,0 +1,9 @@
+package springboot.jpaAdvancedMapping.dao;
+
+import springboot.jpaAdvancedMapping.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+
+}
---
+++ src/main/java/springboot/jpaAdvancedMapping/dao/AppDAOImpl.java
@@ -0,0 +1,17 @@
+package springboot.jpaAdvancedMapping.dao;
+
+import jakarta.persistence.EntityManager;
+import springboot.jpaAdvancedMapping.entity.Instructor;
+
+public class AppDAOImpl implements AppDAO{
+    private EntityManager manager;
+
+    public AppDAOImpl(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void save(Instructor instructor) {
+        manager.persist(instructor);
+    }
+}
